39feac3f17460984c44cf2b090991e799456a28b
aaronb05 <aboyles05@gmail.com>
2022-05-23 10:48:52 -0400

    stage 1

--- api.py
+++ api.py
@@ -1,37 +1,65 @@
 import os
 import requests
 from datetime import datetime
-from dotenv import load_dotenv
+from yaml import safe_load
 
-load_dotenv()
-
-APP_ID = os.getenv("APP_ID")
-APP_KEY = os.getenv("APP_KEY")
-USER = os.getenv("USER")
-PASSWORD = os.getenv("PASSWORD")
+with open('config.yaml', 'r') as file:
+    config = safe_load(file)
 
 
-def post_workout(workout):
+APP_ID = config['fit_api']['app_id']
+APP_KEY = config['fit_api']['app_key']
+USER = config['google_sheet']['username']
+PASSWORD = config['google_sheet']['password']
+AARON_G = "male"
+AARON_W = 78.02
+AARON_H = 188
+AARON_A = 31
+TORI_G = "female"
+TORI_W = 58.51
+TORI_H = 157.5
+TORI_A = 28
+
+
+def get_results(workout, user):
     nutrition_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
     headers = {
         "x-app-id": APP_ID,
         "x-app-key": APP_KEY,
         "content-type": "application/json"
     }
-    params = {
-        "query": workout,
-        "gender": "male",
-        "weight_kg": 78.02,
-        "height_cm": 1.88,
-        "age": 31
-    }
+    if user == "Aaron Boyles":
+        params = {
+            "query": workout,
+            "gender": AARON_G,
+            "weight_kg": AARON_W,
+            "height_cm": AARON_H,
+            "age": AARON_A
+        }
+    elif user == "Tori Boyles":
+        params = {
+            "query": workout,
+            "gender": TORI_G,
+            "weight_kg": TORI_W,
+            "height_cm": TORI_H,
+            "age": TORI_A
+        }
+    else:
+        params = {
+            "query": workout,
+            "gender": AARON_G,
+            "weight_kg": AARON_W,
+            "height_cm": AARON_H,
+            "age": AARON_A
+        }
     post = requests.post(url=nutrition_endpoint, json=params, headers=headers)
     post.raise_for_status()
     response = post.json()
+    print(post.text)
     return response
 
 
-def add_to_sheet(exercise, calories, duration):
+def add_to_sheet(exercise, calories, duration, user):
     today = datetime.now().date().strftime("%m/%d/%Y")
     current_time = datetime.now().time().strftime("%I:%M %p")
     add_endpoint = "https://api.sheety.co/f1d4d46bfaa887c14bc9043c594ef651/workoutTracking/sheet1"
@@ -39,6 +67,7 @@ def add_to_sheet(exercise, calories, duration):
         "sheet1": {
             "date": today,
             "time": current_time,
+            "person": user,
             "exercise": exercise,
             "duration": duration,
             "calories": calories
--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-import api
+from api import get_results, add_to_sheet
+from flask import Flask, render_template, request, redirect
 
-query_text = input("What did you do today: ")
-
-workout_results = config.post_workout(workout=query_text)
-print(workout_results)
-
-calories = float(workout_results["exercises"][0]["nf_calories"])
-activity = str(workout_results["exercises"][0]["name"]).title()
-duration = str(workout_results["exercises"][0]["duration_min"])
-print(calories)
-print(activity)
-print(duration)
-config.add_to_sheet(exercise=activity, calories=calories, duration=duration)
+app = Flask(__name__)
 
 
+@app.route('/post_workout', methods=["POST"])
+def post_workout():
+
+    # Get workout data
+    workout = request.form["work_out"]
+    user = request.form["user"]
+    workout_results = get_results(workout=workout, user=user)
+
+    # Parse workout details
+    calories = float(workout_results["exercises"][0]["nf_calories"])
+    activity = str(workout_results["exercises"][0]["name"]).title()
+    duration = str(workout_results["exercises"][0]["duration_min"])
+
+    # Add workout to google sheet
+    add_to_sheet(user=user, exercise=activity, calories=calories, duration=duration)
+    return redirect("https://docs.google.com/spreadsheets/d/1MtPsT8bg-GZpLnHVlCNj5YKAwRsliFeCOcghJAuZ8C8/edit#gid=0")
 
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
