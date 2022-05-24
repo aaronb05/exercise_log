caf21ac53a10747876fb746721066a20ccbbdab1
aaronb05 <aboyles05@gmail.com>
2022-05-24 08:14:11 -0400

    add smtp email

--- api.py
+++ api.py
@@ -1,28 +1,24 @@
 import os
 import requests
 from datetime import datetime
-from yaml import safe_load
+from smtplib import SMTP, SMTPResponseException, SMTPAuthenticationError
+from dotenv import load_dotenv
 
-# with open('config.yaml', 'r') as file:
-#     config = safe_load(file)
-# APP_ID = config['fit_api']['app_id']
-# APP_KEY = config['fit_api']['app_key']
-# USER = config['google_sheet']['username']
-# PASSWORD = config['google_sheet']['password']
-
+load_dotenv()
 
 APP_ID = os.getenv("app_id")
 APP_KEY = os.getenv("app_key")
 USER = os.getenv("username")
 PASSWORD = os.getenv("password")
-
+GMAIL = os.getenv("gmail")
+GMAIL_KEY = os.getenv("gmail_key")
 AARON_G = "male"
 AARON_W = 78.02
 AARON_H = 188
 AARON_A = 31
 TORI_G = "female"
 TORI_W = 58.51
-TORI_H = 157.5
+TORI_H = 155
 TORI_A = 28
 
 
@@ -60,7 +56,6 @@ def get_results(workout, user):
     post = requests.post(url=nutrition_endpoint, json=params, headers=headers)
     post.raise_for_status()
     response = post.json()
-    print(post.text)
     return response
 
 
@@ -81,6 +76,24 @@ def add_to_sheet(exercise, calories, duration, user):
     auth = (USER, PASSWORD)
     post = requests.post(url=add_endpoint, json=data, auth=auth)
     post.raise_for_status()
-    print(post.text)
 
+
+def smtp_email(from_addr, email_message, name):
+    try:
+        with SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=GMAIL, password=GMAIL_KEY)
+            connection.sendmail(
+                from_addr=from_addr,
+                to_addrs=GMAIL,
+                msg=f"Subject: New Inquiry from {name} at {from_addr}!\n\n"
+                    f"{email_message}"
+            )
+        return True
+    except SMTPAuthenticationError as e:
+        print(e)
+        return "invalid credentials"
+    except SMTPResponseException as e:
+        print(e)
+        return False
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from api import get_results, add_to_sheet
+from api import get_results, add_to_sheet, smtp_email
 from flask import Flask, render_template, request, redirect
+
 
 app = Flask(__name__)
 
@@ -25,6 +26,28 @@ def post_workout():
     # Add workout to google sheet
     add_to_sheet(user=user, exercise=activity, calories=calories, duration=duration)
     return redirect("https://docs.google.com/spreadsheets/d/1MtPsT8bg-GZpLnHVlCNj5YKAwRsliFeCOcghJAuZ8C8/edit#gid=0")
+
+
+@app.route('/send_email', methods=["POST"])
+def send_email():
+    # Gather info from form
+    from_email = request.form["email"]
+    email_message = request.form["message"]
+    name = request.form["name"]
+
+    if from_email == "" or name == "" or email_message == "":
+        return render_template("email_failure.html", email=from_email, name=name, message=email_message)
+
+    email = smtp_email(name=name, from_addr=from_email, email_message=email_message)
+    print(email)
+    if email:
+        return render_template('index.html', message="Email sent successfully!")
+    elif email == "invalid credentials":
+        return render_template('email_failure.html', message="Oops, looks there is a problem on our end! "
+                                              "Please check back at a later time")
+    elif not email:
+        return render_template('email_failure', message="Sorry, we are unable to process your request at this time,"
+                                              "Please try again later")
 
 
 if __name__ == "__main__":
